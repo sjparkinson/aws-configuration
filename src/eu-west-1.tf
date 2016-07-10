3c779ea24a7463a28a9381634770efea0e8acb9c
Samuel Parkinson <sam.james.parkinson@gmail.com>
2016-07-10 18:33:58 +0100

    Output key pair name.

--- src/eu-west-1.tf
+++ src/eu-west-1.tf
@@ -10,11 +10,15 @@ provider "aws" {
  * Outputs
  */
 output "public_subnet_ids" {
-    value = "${join(aws_subnet.public_subnet.*.id, ",")}"
+    value = "${join(",", aws_subnet.public_subnet.*.id)}"
 }
 
 output "private_subnet_ids" {
-    value = "${join(aws_subnet.private_subnet.*.id, ",")}"
+    value = "${join(",", aws_subnet.private_subnet.*.id)}"
+}
+
+output "key_name" {
+    value = "${aws_key_pair.default.key_name}"
 }
 
 /**
@@ -227,6 +231,6 @@ resource "aws_security_group" "default" {
  * @see https://www.terraform.io/docs/providers/aws/r/key_pair.html
  */
 resource "aws_key_pair" "default" {
-    key_name = "Default eu-west-1 Key Pair"
+    key_name = "${format("kp-%.8s", sha1("eu-west-1"))}"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC268iroYGS1LlSKsvT+ZhlYVMkh8DDRpWiSMUHoHTMREfmq/H1m7rZC1AMOjPQn3lg7fWsfGOpe2QfK6+1/WAg5z8B+lXOPB0dZZ39mME46P8FpQCRbSBXcI6qIBkx9kIJP+psbILrYIlBLBT+JaPcXm/0RDrwhlmYpMPliu4Cyxf5Rpv/dhvcLPptnI0//xhvXhyXQOix0NdGX9CVHjrk/KOE7+NVmyf5dKBsEjbgR8rLH2v6iH99Ua9gv9QC9sBLPBHzIDlW97dVG1BepLrAS7W6nQKSMK4pEJiKV9AHGpdPPpShd19KpBoKBqyCjHYstZXlyTfffqUk3M6F6DQ/ eu-west-1"
 }
