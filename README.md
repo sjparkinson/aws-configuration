e268cd2130814861e42ac97961f5fc57c97b4fe7
Samuel Parkinson <sam.james.parkinson@gmail.com>
2016-07-10 17:51:00 +0100

    Shuffle things around after enabling Terraform remote state.

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ VPCs under managment are `eu-west-1`.
 
 ## Configure Terraform Remote State
 
+After cloning this repository run the following to configure remote state.
+
 ```
 terraform remote config \
     -backend=s3 \
     -backend-config="bucket=mainthread-technology-logs" \
-    -backend-config="key=terraform/eu-west-1/terraform.tfstate" \
+    -backend-config="key=terraform/aws-configuration/terraform.tfstate" \
     -backend-config="region=eu-west-1"
 ```
--- src/eu-west-1/ec2-keys.tf
+++
@@ -1,7 +0,0 @@
-/**
- * @see https://www.terraform.io/docs/providers/aws/r/key_pair.html
- */
-resource "aws_key_pair" "default" {
-    key_name = "Default eu-west-1 Key Pair"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC268iroYGS1LlSKsvT+ZhlYVMkh8DDRpWiSMUHoHTMREfmq/H1m7rZC1AMOjPQn3lg7fWsfGOpe2QfK6+1/WAg5z8B+lXOPB0dZZ39mME46P8FpQCRbSBXcI6qIBkx9kIJP+psbILrYIlBLBT+JaPcXm/0RDrwhlmYpMPliu4Cyxf5Rpv/dhvcLPptnI0//xhvXhyXQOix0NdGX9CVHjrk/KOE7+NVmyf5dKBsEjbgR8rLH2v6iH99Ua9gv9QC9sBLPBHzIDlW97dVG1BepLrAS7W6nQKSMK4pEJiKV9AHGpdPPpShd19KpBoKBqyCjHYstZXlyTfffqUk3M6F6DQ/ eu-west-1"
-}
